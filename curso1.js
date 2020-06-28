057d3276f372cb0d36343a9471622e8e7ac9476b
Mario-Ospino <maanosro@gmail.com>
2020-06-28 16:51:33 -0500

    objetos parte 1

--- curso1.js
+++ curso1.js
@@ -1,4 +1,4 @@
-
+/*
 //Manejos con string
 var nombre = 'Mario' , apellido = 'Ospino'
 var nombreenmayusculas= nombre.toUpperCase() //nombre en mayusculas
@@ -6,7 +6,7 @@ var apellidoenminusculas = apellido.toLowerCase()//nombre en minusculaa
 var primeraletraapellido = apellido.charAt(0)// primera letra del apellido en la posicion 0
 var cantidaddeletrasnombre = nombre.length // atributo para saber la cantidad de letras
 var nombrecompleto = nombre + ' ' + apellido //concadenar string
-/*
+
 //interpolacion de texto
 var nombrecompletointerpolado =  `${nombre} ${apellido.toUpperCase()}`//dentro de las llaves se puede poner codigo js
 
@@ -49,7 +49,6 @@ function imprimirNombreEnMayuscula(){
 }
 imprimirNombreEnMayuscula()
 
-
 //modificanco de forma local sin afectar la global
 function imprimirNombreEnMinuscula(nombre){
   //window.nombre asi si queremos hacer referencia a la global
@@ -58,3 +57,39 @@ function imprimirNombreEnMinuscula(nombre){
 }
 imprimirNombreEnMinuscula(nombre)
 */
+
+
+//objetos
+//creamos los objetos
+var sacha = {
+  nombre: 'Sancha',//atributos del objeto
+  apellido: 'grey',
+  edad: 28
+
+}
+var dario = {
+  nombre: 'Dario',
+  apellido: 'Ospino',
+  edad: 27
+}
+
+// manejo de objetos con funciones
+function imprimirNombreEnMinuscula(persona){
+  var nombre = persona.nombre.toLowerCase()//accedemos al atributo nombre el objeto que se envia
+  console.log(nombre)
+  //forma corta console.log(persona.nombre.toLowerCase())
+}
+
+//enviamos los objetos
+imprimirNombreEnMinuscula(dario)
+imprimirNombreEnMinuscula(sacha)
+
+//accediendo directamente a sus atributos como parametros
+function imprimirformacorta({nombre}){
+  console.log(nombre.toUpperCase())
+}
+
+imprimirformacorta(dario)
+imprimirformacorta({nombre:'pepito'})
+
+//desestructurar objetos
