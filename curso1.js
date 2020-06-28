5322927980d934fe9a750f91fb5f3576cd4090cc
Mario-Ospino <maanosro@gmail.com>
2020-06-28 00:06:56 -0500

    Uso de variables string

---
+++ curso1.js
@@ -0,0 +1,19 @@
+/*
+//Manejos con string
+var nombre = 'Mario' , apellido = 'Ospino'
+var nombreenmayusculas= nombre.toUpperCase() //nombre en mayusculas
+var apellidoenminusculas = apellido.toLowerCase()//nombre en minusculaa
+var primeraletraapellido = apellido.charAt(0)// primera letra del apellido en la posicion 0
+var cantidaddeletrasnombre = nombre.length // atributo para saber la cantidad de letras
+var nombrecompleto = nombre + ' ' + apellido //concadenar string
+
+//interpolacion de texto
+var nombrecompletointerpolado =  `${nombre} ${apellido.toUpperCase()}`//dentro de las llaves se puede poner codigo js
+
+var str = nombre.substr(1,3) // tomar desde la posicion 1 y cuantos espacios depues de ahi tomar (3 espacios)
+
+//Reto - mostrar la ultima letra del nombre
+
+var ultimaletranombre = nombre.charAt(nombre.length - 1)
+console.log('Tu nombre es '+ nombre + ' y la ultima letra es "'+ ultimaletranombre+'".')
+*/
